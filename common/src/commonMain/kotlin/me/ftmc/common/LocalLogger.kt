d17581af01d82c659f9382a85173265864995252
Laevatein Scarlet <fish_magic@ftmc.me>
2022-04-08 17:05:08 +0800

    为本地存储器增加大小限制

--- common/src/commonMain/kotlin/me/ftmc/common/LocalLogger.kt
+++ common/src/commonMain/kotlin/me/ftmc/common/LocalLogger.kt
@@ -1,5 +1,8 @@
 package me.ftmc.common
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import kotlinx.serialization.Serializable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -12,11 +15,15 @@ class LocalLogger {
   companion object {
     private val backendLogger: Logger = LoggerFactory.getLogger("FrontEnd")
     val loggerBuket = mutableListOf<LogObject>()
+    var maxSize by mutableStateOf(100)
   }
 
   @Synchronized
   fun info(message: String) {
     synchronized(loggerBuket) {
+      if (maxSize > 0 && loggerBuket.size >= maxSize) {
+        loggerBuket.removeAt(0)
+      }
       backendLogger.info(message)
       loggerBuket.add(LogObject("INFO", Instant.now().epochSecond, message))
     }
@@ -25,6 +32,9 @@ class LocalLogger {
   @Synchronized
   fun warn(message: String) {
     synchronized(loggerBuket) {
+      if (maxSize > 0 && loggerBuket.size >= maxSize) {
+        loggerBuket.removeAt(0)
+      }
       backendLogger.warn(message)
       loggerBuket.add(LogObject("WARN", Instant.now().epochSecond, message))
     }
@@ -33,6 +43,9 @@ class LocalLogger {
   @Synchronized
   fun debug(message: String) {
     synchronized(loggerBuket) {
+      if (maxSize > 0 && loggerBuket.size >= maxSize) {
+        loggerBuket.removeAt(0)
+      }
       backendLogger.debug(message)
       loggerBuket.add(LogObject("DEBUG", Instant.now().epochSecond, message))
     }
